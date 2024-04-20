bcfb566cb38053cbc3bd426e078de29bbea3eaab
siriusgurban <failoglu96@gmail.com>
2024-04-20 20:11:43 +0400

    update

--- src/components/CoinTask/index.jsx
+++ src/components/CoinTask/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 const endpoint = 'https://en.numista.com/catalogue/photos/azerbaidjan/'
+const defaultImg =
+  'https://media-s3-us-east-1.ceros.com/woundedwarriorproject/images/2023/05/17/7eae891d05abcc202e3c32c178b4f608/nickel.png'
 
 function CoinTask() {
   const [count, setCount] = useState(0)
@@ -13,15 +15,16 @@ function CoinTask() {
 
   function onCoin() {
     setCount((count) => count + 1)
+    onCoinChange()
   }
 
-  useEffect(() => {
+  function onCoinChange() {
     const rndNumber = Math.floor(Math.random() * 2)
     setImg(coinsArr[rndNumber])
     rndNumber == 0
       ? setFace((count) => count + 1)
       : setBack((count) => count + 1)
-  }, [count])
+  }
 
   return (
     <div>
@@ -31,7 +34,12 @@ function CoinTask() {
         Face: {face}, Back: {back}
       </h2>
       <div>
-        <img src={img} alt="arxa-uz" width={200} height={200} />
+        <img
+          src={img == undefined ? defaultImg : img}
+          alt="arxa-uz"
+          width={200}
+          height={200}
+        />
       </div>
       <button onClick={onCoin}>Flip</button>
     </div>
